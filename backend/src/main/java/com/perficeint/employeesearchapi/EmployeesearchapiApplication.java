dc97d05b3e3f21b819b46ba4d077448b22407f6e
Samuel Luswata <lusamdebonfils@users.noreply.github.com>
2020-03-09 18:03:06 -0500

    Initial commit
    created start.io project
    dependencies added web,h2,jpa and lombok

---
+++ backend/src/main/java/com/perficeint/employeesearchapi/EmployeesearchapiApplication.java
@@ -0,0 +1,13 @@
+package com.perficeint.employeesearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesearchapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesearchapiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/perficeint/employeesearchapi/EmployeesearchapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficeint.employeesearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesearchapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
